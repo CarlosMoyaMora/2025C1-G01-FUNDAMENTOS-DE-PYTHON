3934632935279a7ad31212695df64199c624c597
Carlos Moya Mora <moyabmx1996@gmail.com>
2025-03-02 15:16:41 -0600

    update

---
+++ Clases/clase 06/prueba.py
@@ -0,0 +1,21 @@
+#ENTRADAS
+while True:
+    try:
+        precio_producto = float(input('favor ingrece el precio de el primer producto'))
+        while (precio_producto) : input('deseas ingresar otro precio')
+        if pregunta != ('si') .lawer:
+           nuevo_precio = float(input('ingrese el precio'))
+        
+        
+    except ValueError:
+        print('Error: por favor ingrese un valor numerico correcto.')
+
+
+x = int(input())
+y = int(input())
+ 
+x = x % y
+x = x % y
+y = y % x
+ 
+print(y)
