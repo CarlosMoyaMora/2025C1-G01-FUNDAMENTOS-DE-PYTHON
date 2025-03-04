321d2b09552c6ab43da31919735ea5ea7413bd7a
Carlos Moya Mora <moyabmx1996@gmail.com>
2025-03-04 17:06:55 -0600

    update 03

--- Clases/clase 06/prueba.py
+++ Clases/clase 06/prueba.py
@@ -1,13 +1,10 @@
-#ENTRADAS
-while True:
-    try:
-        precio_producto = float(input('favor ingrece el precio de el primer producto'))
-        while (precio_producto) : input('deseas ingresar otro precio')
-        if pregunta != ('si') .lawer:
-           nuevo_precio = float(input('ingrese el precio'))
-        
-        
-    except ValueError:
-        print('Error: por favor ingrese un valor numerico correcto.')
-
-
+try:
+    precio_producto = float(input('favor ingrece el precio de el primer producto'))    
+    while (precio_producto):
+        nuevo_prod = str(input('Si desea ingresar un nuevo producto escriba si, de los contrario escriba no'))
+        if nuevo_prod != 'si' .lower:
+            else nuevo_prod
+            break
+    print('gracias por su compra')           
+except ValueError:
+    print('Error: por favor ingrese un valor numerico correcto.')
--- Clases/clase 06/tempCodeRunnerFile.py
+++ Clases/clase 06/tempCodeRunnerFile.py
@@ -1,8 +1,11 @@
-x = int(input())
-y = int(input())
- 
-x = x % y
-x = x % y
-y = y % x
- 
-print(y)
+try:
+    precio_producto = float(input('favor ingrece el precio de el primer producto'))    
+    while (precio_producto):
+        nuevo_prod = str(input('Si desea ingresar un nuevo producto escriba si, de los contrario escriba no'))
+        if nuevo_prod != 'si' .lower:
+            while (nuevo_prod):
+                nuevo_precio = float(input('ingrese el precio'))
+            break
+    print('gracias por su compra')           
+except ValueError:
+    print('Error: por favor ingrese un valor numerico correcto.')
