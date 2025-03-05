ae2a1ce77946b7dace91e3d6e184434fed2271d7
Carlos Moya Mora <moyabmx1996@gmail.com>
2025-03-04 18:46:28 -0600

    Update prueba.py

--- Clases/clase 06/prueba.py
+++ Clases/clase 06/prueba.py
@@ -1,10 +1,9 @@
 #ENTRADAS
 print('porfavor ingrese un numero para mostrar su tabla del uno al 10')
 numero_usuario = int(input())
-multiplicacion = 1
+multiplicacion = 5
 #PROCESO
 while(numero_usuario):
    if multiplicacion < 0 and multiplicacion <= 10:
             operacion = numero_usuario * multiplicacion
             print(f'{numero_usuario} x {multiplicacion} = {operacion}')
-            break
