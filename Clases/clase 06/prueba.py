d5f2e1822df3981f693ccde048b09780fab867ba
Carlos Moya Mora <moyabmx1996@gmail.com>
2025-03-12 18:07:30 -0600

    updata

--- Clases/clase 06/prueba.py
+++ Clases/clase 06/prueba.py
@@ -1,13 +1,2 @@
-
-pregunta = True #al ser una condicion que no requiere un valor, le asignamos true.
-articulo_1 = float(input('Porfavor, ingrese el precio de el primer atriculo:'))
-pregunta = str(input('Desea ingresar otro articulo?:')) # pregunta que nos dira si entramos en el bucle o no
-while (pregunta == 'si' .lower()): # condicion que se debe cumplir para que entremos o salgamos de el bucle
-    precio_2 = float(input('Ingrese el nuevo producto:'))
-    nuevo_precio = articulo_1 + precio_2
-    articulo_1 = nuevo_precio
-    pregunta = str(input('Desea ingresar otro articulo?:'))    
-
-        
-print(f'el precio final es: {articulo_1}')    
- # Finaliza el bucle.
+my_list = [[0, 1, 2, 3] for i in range(2)]
+print(my_list[2][0])
--- Clases/clase 06/tempCodeRunnerFile.py
+++ Clases/clase 06/tempCodeRunnerFile.py
@@ -1,10 +1,3 @@
-precio_producto = float(input('Porfavor ingrese el priecio de el producto:'))
-nuevo_prod =input('Si desea ingresar un nuevo producto escriba si, de los contrario escriba no')
-nuevo_prod != 'si'
-while(nuevo_prod):
-   nuevo_precio = float(input('Ingrese el precio de el nuevo articulo'))
-   suma_productos = precio_producto + nuevo_precio
-   precio_producto = suma_productos
-   repetir = input('Si desea ingresar un nuevo producto escriba si, de los contrario escriba no')
-   if repetir != 'si':
-      print(F'Gracias por su compra, Total a pagar: {precio_producto}')     
+nums = [1, 2, 3]
+vals = nums
+del vals[1:2]
